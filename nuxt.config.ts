867a3a62619b14a7fd00be18c6455f3cf56ab4f2
rama sulthan <76695790+ramasedang@users.noreply.github.com>
2023-02-11 22:30:01 +0700

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,39 @@
+import { defineNuxtConfig } from 'nuxt/config'
+import ViteComponents from 'unplugin-vue-components/vite'
+import IconsResolver from 'unplugin-icons/resolver'
+
+export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'A Better Nuxt 3 Starter',
+      link: [{ rel: 'icon', type: 'image/*', href: '/favicon.svg' }]
+    }
+  },
+  modules: [
+    'nuxt-windicss',
+    '@pinia/nuxt',
+    'unplugin-icons/nuxt',
+    '@vueuse/nuxt',
+    [
+      '@intlify/nuxt3',
+      {
+        localeDir: 'locales',
+        vueI18n: {
+          locale: 'en'
+        }
+      }
+    ]
+  ],
+  vite: {
+    plugins: [
+      ViteComponents({
+        resolvers: [
+          IconsResolver({
+            componentPrefix: ''
+          })
+        ],
+        dts: true
+      })
+    ]
+  }
+})
